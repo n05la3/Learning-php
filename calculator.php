11f31109b755f23944fb8247aee180b745b7424c
Calson Noah <chiacalson@gmail.com>
2017-08-11 22:22:19 +0100

    extended calculator class

--- calculator.php
+++ calculator.php
@@ -1,8 +1,8 @@
 <?php
 class Calculator 
 {
-    private $num1;
-    private $num2;
+    protected $num1;
+    protected $num2;
     
     public function __construct($n,$m)
     {
@@ -18,9 +18,39 @@ class Calculator
     public function multiply()
     {
         return $this->num1*$this->num2;
-    }        
+    }    
+    
+    public function subtract()
+    {
+        return $this->num1-$this->num2;
+    }
+    
+    public function divide()
+    {
+        return $this->num1/$this->num2;
+    }
 }
 
-$calc = new Calculator(3,4);
-echo "3 and 4 sum up to: ". $calc->add(). "<br>";
-echo "multiplying 3 and 4 gives: ". $calc->multiply(). "<br>";
+class Advance_calc extends Calculator
+{
+    private static $_allowed_functions = array('pow'=>2, 'sqrt'=>1, 'exp'=>1);
+    public function __construct($arg1,$arg2=null)
+    {
+        parent::__construct($arg1,$arg2);
+    }
+    
+    public function __call($method, $func_param)
+    {
+        if(!empty($func_param))
+                die("Pass the arguments when the object is created");
+        if(in_array($method, array_keys(Advance_calc::$_allowed_functions), TRUE))
+        { 
+            $func_param = array($this->num2);
+            if(Advance_calc::$_allowed_functions[$method]===2)
+                array_push ($func_param, $this->num2);
+            return call_user_func_array($method, $func_param);
+        }
+    }   
+}
+$my_calc = new Advance_calc(3,4);
+echo $my_calc->pow();
