6c9278f8d01cc5a0403ca49b09be26a43a19698b
Calson Noah <chiacalson@gmail.com>
2017-08-29 08:03:50 +0100

    added config constants

---
+++ book_club/config.php
@@ -0,0 +1,8 @@
+<?php 
+define('DB_DSN', 'mysql:host=localhost,dbname=mydatabase');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'Ddon_1020?');
+define('PAGE_SIZE', 5);
+define('TABL_MEMBERS', 'members');
+define('TABL_ACCESS_LOG', 'accessLog');
+?>
