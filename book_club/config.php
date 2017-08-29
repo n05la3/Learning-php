e6d540c4bb3a0d9255af8bd803c97e163a238414
Calson Noah <chiacalson@gmail.com>
2017-08-29 19:42:03 +0100

    added a comment

--- book_club/config.php
+++ book_club/config.php
@@ -1,8 +1,8 @@
 <?php 
-define('DB_DSN', 'mysql:host=localhost,dbname=mydatabase');
+define('DB_DSN', 'mysql:dbname=mydatabase');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'Ddon_1020?');
+define('DB_PASSWORD', 'mypass');//replace mypass with your db password
 define('PAGE_SIZE', 5);
-define('TABL_MEMBERS', 'members');
-define('TABL_ACCESS_LOG', 'accessLog');
+define('TBL_MEMBERS', 'members');
+define('TBL_ACCESS_LOG', 'accessLog');
 ?>
