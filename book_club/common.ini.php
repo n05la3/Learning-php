4e5a941618147639e7aacec96f9b68c9db90b78a
Calson Noah <chiacalson@gmail.com>
2017-09-08 21:43:58 +0100

    updated common.ini script

--- book_club/common.ini.php
+++ book_club/common.ini.php
@@ -1,5 +1,5 @@
 <?php
-function display_page_header()
+function displayPageHeader()
 { ?>
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -15,11 +15,11 @@ function display_page_header()
 		</style>
 	</head>
 	<body>
-		<h1><?= echo $page_title; ?></h1>
+		<h1><?php echo $page_title; ?></h1>
 	</body>
 <?php 
 }
-function display_page_footer()
+function displayPageFooter()
 {	
 ?>
 	</body>
