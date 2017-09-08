d52fb575f543e35a7204a8fefdb48d92eed9e7cf
Calson Noah <chiacalson@gmail.com>
2017-09-08 21:43:23 +0100

    update config class

--- book_club/config.php
+++ book_club/config.php
@@ -1,7 +1,7 @@
 <?php 
 define('DB_DSN', 'mysql:dbname=mydatabase');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'mypass');//replace mypass with your db password
+define('DB_PASSWORD', 'Ddon_1020?');//replace mypass with your db password
 define('PAGE_SIZE', 5);
 define('TBL_MEMBERS', 'members');
 define('TBL_ACCESS_LOG', 'accessLog');
