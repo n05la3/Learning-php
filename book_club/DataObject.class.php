fc63f7152a4546510fa447744a0fd839eb8cd24b
Calson Noah <chiacalson@gmail.com>
2017-09-08 21:42:48 +0100

    update dataObject class

--- book_club/DataObject.class.php
+++ book_club/DataObject.class.php
@@ -6,15 +6,15 @@ require_once "config.php";
 abstract class DataObject 
 {
 	protected $data = array();
-	public function __construct( $data ) {
+
+	public function __construct($data) {
 		foreach ( $data as $key => $value ) {
-			if ( array_key_exists( $key, $this-> data )) 
-				$this- > data[$key] = $value;
+			if ( array_key_exists( $key, $this -> data)) 
+				$this -> data[$key] = $value;
 			}
 	}
 
-	public function getValue( $field ) 
-	{
+	public function getValue( $field ){
 		if ( array_key_exists( $field, $this-> data ) ) {
 			return $this-> data[$field];
 		} else {
@@ -27,9 +27,9 @@ abstract class DataObject
 	//connecting to mysql
 	protected function connect() {
 		try{
-			$conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
-			$conn- > setAttribute( PDO::ATTR_PERSISTENT, true );
-			$conn- > setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+			$conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD);
+			$conn-> setAttribute( PDO::ATTR_PERSISTENT, true );//using ATTR_PERSISTENT; connection stays open for use by other parts of applicaton
+			$conn-> setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 		}catch ( PDOException $e ) {
 			die( "Connection failed: " . $e->getMessage() );
 		}
