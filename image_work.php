96b1297d50d4e376dd98d2aa1c4375ea78c9733d
Calson Noah <chiacalson@gmail.com>
2017-09-12 16:09:08 +0100

    watermark effect

--- image_work.php
+++ image_work.php
@@ -1,11 +1,37 @@
 <?php 
 	
-	$image = imagecreatefromjpeg("test.jpg");
-	if(file_exists("test.jpg")){
-		header( "Content-type: image/jpeg" );
-		imagejpeg($image);
-		imagedestroy($image);
-	}
-	else 
-		die("file not found");
-	?>
+if(file_exists("noslac.jpg") && file_exists("copyright.png")){
+	/*$myImage = imagecreatefromjpeg( "noslac.jpg" );
+	$myCopyright = imagecreatefrompng( "copyright.png" );
+	$destWidth = imagesx( $myImage );
+	$destHeight = imagesy( $myImage );
+	$srcWidth = imagesx( $myCopyright );
+	$srcHeight = imagesy( $myCopyright );
+	$destX = ($destWidth - $srcWidth) / 2;
+	$destY = ($destHeight - $srcHeight) / 2;
+	imagecopy( $myImage, $myCopyright, $destX, $destY, 0, 0, $srcWidth,
+	$srcHeight );
+	header( "Content-type: image/jpeg" );
+	imagejpeg( $myImage );
+	imagedestroy( $myImage );
+	imagedestroy( $myCopyright );*/	
+
+	
+	$image = imagecreatefromjpeg("noslac.jpg");
+	$copyright = imagecreatefrompng("copyright.png");
+	$src_width = imagesx($copyright);
+	$src_height = imagesy($copyright);
+	$dest_width = imagesx($image);
+	$dest_height = imagesy($image);
+	$destX = ($dest_width - $src_width)/2;
+	$destY = ($dest_height - $src_height)/2;
+	imagecopy($image, $copyright, $destX, $destY, 0, 0, $src_width, $src_height);
+	header( "Content-type: image/jpeg" );
+	imagejpeg($image);
+	imagedestroy($image);
+	imagedestroy($copyright);
+}
+else 
+	die("file not found");
+
+?>
